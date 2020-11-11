bedbdfbbf593139aaf9d5d955b1c854f594b16f4
MaikGon <michal.gontarczyk@wp.pl>
2020-11-11 20:35:09 +0100

    Task7

--- project_1.py
+++ project_1.py
@@ -132,18 +132,113 @@ def zad6(data):
     top_data_f = pd.DataFrame(stacked_f, columns=['name', 'number'])
     sorted_f = top_data_f.groupby(by='name').sum().sort_values('number', ascending=False)
     yy = sorted_f.iloc[0:1000, :]
-    print("1000 Najpopularniejszych imion zenskich: ", list(yy.index))
+    # print("1000 Najpopularniejszych imion zenskich: ", list(yy.index))
     # yy.to_csv("top_1000_fem.csv")
 
     stacked_m = list(zip(arr_names_m, arr_vals_m))
     top_data_m = pd.DataFrame(stacked_m, columns=['name', 'number'])
     sorted_m = top_data_m.groupby(by='name').sum().sort_values('number', ascending=False)
     xxy = sorted_m.iloc[0:1000, :]
-    print("1000 Najpopularniejszych imion meskich: ", list(xxy.index))
+    # print("1000 Najpopularniejszych imion meskich: ", list(xxy.index))
     # xxy.to_csv("top_1000_mel.csv")
+    return list(yy.index), list(xxy.index)
 
 
-def zad7(data):
+def zad7(data, top_data):
+    Fem_1st = top_data[0][0]
+    Mel_1st = top_data[1][0]
+
+    harr_arr_num = []
+    harr_arr_popul = []
+    harr_arr_year = []
+    Marilin_arr_num = []
+    Marilin_arr_popul = []
+    Marilin_arr_year = []
+    Fem_1st_arr_num = []
+    Fem_1st_arr_popul = []
+    Fem_1st_arr_year = []
+    Mel_1st_arr_num = []
+    Mel_1st_arr_popul = []
+    Mel_1st_arr_year = []
+
+    table = pd.pivot_table(data, values='number', index=['name'], columns=['year'], aggfunc=np.sum, fill_value=0)
+
+    for ind in data['year'].unique():
+        harr_arr_num.append(table[str(ind)]['Harry'])
+        harr_arr_year.append(str(ind))
+        Marilin_arr_num.append(table[str(ind)]['Marilin'])
+        Marilin_arr_year.append(str(ind))
+        Fem_1st_arr_num.append(table[str(ind)][Fem_1st])
+        Fem_1st_arr_year.append(str(ind))
+        Mel_1st_arr_num.append(table[str(ind)][Mel_1st])
+        Mel_1st_arr_year.append(str(ind))
+
+    stacked = list(zip(harr_arr_year, harr_arr_num, Marilin_arr_num, Fem_1st_arr_num, Mel_1st_arr_num))
+    stacked = pd.DataFrame(stacked, columns=['year', 'Harry', 'Marilin', str(Fem_1st), str(Mel_1st)])
+
+    table1 = pd.pivot_table(data, values='number', index=['year'], aggfunc=np.sum, fill_value=0)
+
+    # Frequency
+    for i in data['year'].unique():
+        all_numbers = table1.loc[i]
+
+        val = table[str(i)]['Harry'] / all_numbers
+        harr_arr_popul.append(val)
+        val = table[str(i)]['Marilin'] / all_numbers
+        Marilin_arr_popul.append(val)
+        val = table[str(i)][Fem_1st] / all_numbers
+        Fem_1st_arr_popul.append(val)
+        val = table[str(i)][Mel_1st] / all_numbers
+        Mel_1st_arr_popul.append(val)
+
+    stacked['freq_harr'] = harr_arr_popul
+    stacked['freq_mar'] = Marilin_arr_popul
+    stacked['freq_fem'] = Fem_1st_arr_popul
+    stacked['freq_mel'] = Mel_1st_arr_popul
+
+    print('Ilosc imienia Harry w 1940: ', table['1940']['Harry'])
+    print('Ilosc imienia Harry w 1980: ', table['1980']['Harry'])
+    print('Ilosc imienia Harry w 2019: ', table['2019']['Harry'])
+    print('Ilosc imienia Marilyn w 1940: ', table['1940']['Marilin'])
+    print('Ilosc imienia Marilyn w 1980: ', table['1980']['Marilin'])
+    print('Ilosc imienia Marilyn w 2019: ', table['2019']['Marilin'])
+    print('Ilosc imienia ', Fem_1st, ' w 1940: ', table['1940'][Fem_1st])
+    print('Ilosc imienia ', Fem_1st, ' w 1980: ', table['1980'][Fem_1st])
+    print('Ilosc imienia ', Fem_1st, ' w 2019: ', table['2019'][Fem_1st])
+    print('Ilosc imienia ', Mel_1st, ' w 1940: ', table['1940'][Mel_1st])
+    print('Ilosc imienia ', Mel_1st, ' w 1980: ', table['1980'][Mel_1st])
+    print('Ilosc imienia ', Mel_1st, ' w 2019: ', table['2019'][Mel_1st])
+
+    arr = []
+
+    for i in data['year'].unique():
+        arr.append(int(i))
+
+    fig, axs = plt.subplots(2)
+
+    axs[0].plot(arr, stacked['Harry'], 'r')
+    axs[0].plot(arr, stacked['Marilin'], 'b')
+    axs[0].plot(arr, stacked[Mel_1st], 'g')
+    axs[0].plot(arr, stacked[Fem_1st], 'k')
+    axs[0].legend(['Harry', 'Marilyn', Mel_1st, Fem_1st], loc='upper right')
+    axs[0].set_xlim(1880, 2020)
+    axs[0].set_xticks(np.arange(1880, 2021, 20))
+    axs[0].grid()
+
+    axs[1].plot(arr, stacked['freq_harr'], 'r')
+    axs[1].plot(arr, stacked['freq_mar'], 'b')
+    axs[1].plot(arr, stacked['freq_mel'], 'g')
+    axs[1].plot(arr, stacked['freq_fem'], 'k')
+    axs[1].legend(['Harry', 'Marilyn', Mel_1st, Fem_1st], loc='upper right')
+    axs[1].set_xlim(1880, 2020)
+    axs[1].set_xticks(np.arange(1880, 2021, 20))
+    axs[1].grid()
+
+    plt.subplots_adjust(hspace=0.5)
+    plt.show()
+
+
+def zad10(data):
     pass
 
 
@@ -154,5 +249,7 @@ if __name__ == "__main__":
     # zad4(zad1())
     # zad5(zad1())
     # zad6(zad1())
-    zad7(data)
+    zad7(zad1(), zad6(zad1()))
     # zad8()
+    # zad9()
+    # zad10(zad1())
